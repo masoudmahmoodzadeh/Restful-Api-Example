85b62c0e4a85442193f89092cd4d3bd3583fcd7e
masoud <mahmoodzadeh.masoud@gmail.com>
2022-05-08 12:37:35 +0430

    [STRUCTURE] added base class apis

---
+++ lib/apis/base/api_manager.dart
@@ -0,0 +1,13 @@
+
+import 'package:restful_api_example/apis/base/base_request.dart';
+
+import 'base_api.dart';
+
+class ApiManager extends BaseApi{
+
+  @override
+  void sendRequest(BaseRequest request) {
+    
+  }
+
+}
---
+++ lib/apis/base/base_api.dart
@@ -0,0 +1,6 @@
+import 'package:restful_api_example/apis/base/base_request.dart';
+
+abstract class BaseApi{
+
+  void sendRequest(BaseRequest request);
+}
