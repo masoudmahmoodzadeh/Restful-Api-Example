d9858cf9c9ccd1998ff368559a0b6ff2098222f6
masoud <mahmoodzadeh.masoud@gmail.com>
2022-05-09 14:46:45 +0430

    [REFACTOR] create page for listCrypto

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:restful_api_example/apis/base/api_manager.dart';
-import 'package:restful_api_example/apis/base/response_callback.dart';
-import 'package:restful_api_example/apis/list_crypto/list_crypto_request.dart';
-import 'package:restful_api_example/apis/list_crypto/list_crypto_response.dart';
+import 'package:restful_api_example/screens/crypto_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,79 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: const CryptoScreen(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  List<ListCryptoResponse> listCrypto = [];
-
-  @override
-  void initState() {
-    _getListCrypto();
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Restful Api Example"),
-      ),
-      body: Center(
-        child: listCrypto.isEmpty
-            ? const CircularProgressIndicator()
-            : ListView.separated(
-                padding: const EdgeInsets.all(16),
-                itemCount: listCrypto.length,
-                itemBuilder: (context, index) {
-                  ListCryptoResponse item = listCrypto[index];
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(item.symbol),
-                      const SizedBox(height: 16),
-                      Text(item.price),
-                    ],
-                  );
-                },
-                separatorBuilder: (BuildContext context, int index) {
-                  return const Divider(
-                    color: Colors.blue,
-                    thickness: 1,
-                  );
-                },
-              ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-
-  void _getListCrypto() {
-    ApiManager apiManager = ApiManager();
-    apiManager.sendRequest(ListCryptoRequest(),
-        ResponseCallback<List<ListCryptoResponse>>(
-      onSuccess: ((json) {
-        List<ListCryptoResponse> data = (json as List)
-            .map((item) => ListCryptoResponse.fromJson(item))
-            .toList();
-
-        setState(() {
-          listCrypto = data;
-        });
-      }),
-    ));
   }
 }
---
+++ lib/screens/crypto_screen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+import '../apis/base/api_manager.dart';
+import '../apis/base/response_callback.dart';
+import '../apis/list_crypto/list_crypto_request.dart';
+import '../apis/list_crypto/list_crypto_response.dart';
+
+class CryptoScreen extends StatefulWidget {
+  const CryptoScreen({Key? key}) : super(key: key);
+
+  @override
+  _CryptoScreenState createState() => _CryptoScreenState();
+}
+
+class _CryptoScreenState extends State<CryptoScreen> {
+  List<ListCryptoResponse> listCrypto = [];
+
+  @override
+  void initState() {
+    _getListCrypto();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Restful Api Example"),
+      ),
+      body: Center(
+        child: listCrypto.isEmpty
+            ? const CircularProgressIndicator()
+            : ListView.separated(
+                padding: const EdgeInsets.all(16),
+                itemCount: listCrypto.length,
+                itemBuilder: (context, index) {
+                  ListCryptoResponse item = listCrypto[index];
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(item.symbol),
+                      const SizedBox(height: 16),
+                      Text(item.price),
+                    ],
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) {
+                  return const Divider(
+                    color: Colors.blue,
+                    thickness: 1,
+                  );
+                },
+              ),
+      ),
+    );
+  }
+
+  void _getListCrypto() {
+    ApiManager apiManager = ApiManager();
+    apiManager.sendRequest(ListCryptoRequest(),
+        ResponseCallback<List<ListCryptoResponse>>(
+      onSuccess: ((json) {
+        List<ListCryptoResponse> data = (json as List)
+            .map((item) => ListCryptoResponse.fromJson(item))
+            .toList();
+
+        setState(() {
+          listCrypto = data;
+        });
+      }),
+    ));
+  }
+}
